6eb62daa92a71c55a17ea6550ee941efa2067aa5
David Stosik <david.stosik+git-noreply@gmail.com>
2017-12-04 13:13:05 +0900

    Remove useless spec

--- spec/adventofcode2017_spec.rb
+++ spec/adventofcode2017_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe AdventOfCode2017 do
   it "has a version number" do
     expect(AdventOfCode2017::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
